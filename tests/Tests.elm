741d712515d9c58e7bc89bdda2eef530dce3e54b
Hugo Saracino <hugo.saracino@gmail.com>
2018-09-11 15:24:34 +0200

    :construction: Remove fake tests for the time being
    TODO: Introduce a lot of real test cases! #1

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,23 +2,14 @@ module Tests exposing (suite)
 
 import Expect exposing (equal)
 import Fuzz exposing (int)
-import Main exposing (Msg(..), update)
 import Test exposing (Test, describe, fuzz, test)
 
 
 suite : Test
 suite =
-    describe "Update"
-        [ fuzz int "Increment" <|
-            \n ->
-                update Inc n
-                    |> equal (n + 1)
-        , fuzz int "Decrement" <|
-            \n ->
-                update Dec n
-                    |> equal (n - 1)
-        , fuzz int "Reset" <|
-            \n ->
-                update Reset n
+    describe "Test Suite"
+        [ test "Success" <|
+            \_ ->
+                identity 0
                     |> equal 0
         ]
